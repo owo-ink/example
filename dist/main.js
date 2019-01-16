e3a0d2a320c50e02ac58aa13ada1e8bcdb25f14a
PUGE <mail@puge.me>
2019-01-16 17:25:50 +0800

    save

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,7 @@
     window.ozzx = {
       script: {}
     };
-    var globalConfig = {"root":"/src","entry":"home","headFolder":"head","outFolder":"dist","autoPack":true,"minifyCss":false,"minifyJs":false,"pageFolder":"page","isOnePage":true};
+    var globalConfig = {"root":"/src","entry":"home","headFolder":"head","outFolder":"dist","autoPack":true,"minifyCss":false,"minifyJs":false,"pageFolder":"page","isOnePage":false};
   // 对象合并方法
 function assign(a, b) {
   var newObj = {}
@@ -60,8 +60,10 @@ function pgNameHandler (dom) {
     var clickFunc = tempDom.attributes['@click']
     
     if (clickFunc) {
+      console.log(dom.children)
       tempDom.onclick = function() {
         var clickFor = this.attributes['@click'].textContent
+        console.log(i)
         // 判断页面是否有自己的方法
         var newPageFunction = window.ozzx.script[window.ozzx.activePage]
         
@@ -101,12 +103,62 @@ function pgNameHandler (dom) {
       pgNameHandler(tempDom)
     }
   }
-}// 页面资源加载完毕事件
-window.onload = function() {
-  var page = globalConfig.entry
-  window.ozzx.activePage = page
-  var entryDom = document.getElementById('ox-' + page)
-  runPageFunction(page, entryDom)
+}// 获取URL #后面内容
+function getarg(url){
+  arg = url.split("#");
+  return arg[1];
 }
-      window.ozzx.script = {home:{data:{nameList:{rank1:{name:"lis",like:"orange"},rank2:{name:"kim",like:"yellow"},rank3:{name:"tony",like:"white"}}},created:function created(){console.log('hellow word!');},methods:{showAlert:function showAlert(othersName,myName){console.log(this);alert('Hellow '+othersName+', My name is '+myName);}}}}
+
+// 页面资源加载完毕事件
+window.onload = function() {
+  // 取出URL地址判断当前所在页面
+  var pageArg = getarg(window.location.href)
+  // 从配置项中取出程序入口
+  var page = pageArg ? pageArg : globalConfig.entry
+  if (page) {
+    var entryDom = document.getElementById('ox-' + page)
+    if (entryDom) {
+      // 显示主页面
+      entryDom.style.display = 'block'
+      window.ozzx.activePage = page
+      runPageFunction(page, entryDom)
+    } else {
+      console.error('入口文件设置错误!')
+    }
+  } else {
+    console.error('未设置程序入口!')
+  }
+}
+
+// url发生改变事件
+window.onhashchange = function(e) {
+  var oldUrlParam = getarg(e.oldURL)
+  var newUrlParam = getarg(e.newURL)
+  // 查找页面跳转前的page页(dom节点)
+  // console.log(oldUrlParam)
+  // 如果源地址获取不到 那么一般是因为源页面为首页
+  if (oldUrlParam === undefined) {
+    oldUrlParam = globalConfig.entry
+  }
+  var oldDom = document.getElementById('ox-' + oldUrlParam)
+  if (oldDom) {
+    // 隐藏掉旧的节点
+    oldDom.style.display = 'none'
+  }
+  // 查找页面跳转后的page
+  
+  var newDom = document.getElementById('ox-' + newUrlParam)
+  // console.log(newDom)
+  if (newDom) {
+    // 隐藏掉旧的节点
+    newDom.style.display = 'block'
+  } else {
+    console.error('页面不存在!')
+    return
+  }
+  window.ozzx.activePage = newUrlParam
+  runPageFunction(newUrlParam, entryDom)
+}
+
+      window.ozzx.script = {home:{data:{nameList:{rank1:{name:"lis",like:"orange"},rank2:{name:"kim",like:"yellow"},rank3:{name:"tony",like:"white"}}},created:function created(){console.log('hellow word!');},methods:{showAlert:function showAlert(othersName,myName){console.log('可以传递参数:',othersName,myName);console.log('方便的获取到可能会用到的信息!');console.log(this);}}},name:{created:function created(){console.log('my name is pack!');}}}
     
