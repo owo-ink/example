42987cfc4d53885bebc33aeb6a9e7bc12cf3ee58
PUGE <100284685@qq.com>
2019-01-17 23:50:52 +0800

    支持页面切换效果(淡入,淡出)

--- dist/main.js
+++ dist/main.js
@@ -79,6 +79,19 @@ function pgNameHandler (dom) {
         if (parameterList && parameterList.length > 0) {
           // 参数列表
           parameterArr = parameterList[0].split(',')
+          // 进一步处理参数
+          for (let i = 0; i < parameterArr.length; i++) {
+            var parameterValue = parameterArr[i].replace(/(^\s*)|(\s*$)/g, "")
+            // console.log(parameterValue)
+            // 判断参数是否为一个字符串
+            if (parameterValue.charAt(0) === '"' && parameterValue.charAt(parameterValue.length - 1) === '"') {
+              parameterArr[i] = parameterValue.substring(1, parameterValue.length - 2)
+            }
+            if (parameterValue.charAt(0) === "'" && parameterValue.charAt(parameterValue.length - 1) === "'") {
+              parameterArr[i] = parameterValue.substring(1, parameterValue.length - 2)
+            }
+            // console.log(parameterArr[i])
+          }
           clickFor = clickFor.replace('(' + parameterList + ')', '')
         }
         // console.log(newPageFunction)
@@ -87,7 +100,7 @@ function pgNameHandler (dom) {
           // 绑定window.ozzx对象
           // console.log(tempDom)
           newPageFunction.methods[clickFor].apply({
-            $el: tempDom,
+            $el: this,
             activePage: window.ozzx.activePage,
             domList: window.ozzx.domList,
             data: newPageFunction.data,
@@ -110,7 +123,7 @@ function getarg(url){
 // 页面资源加载完毕事件
 window.onload = function() {
   // 取出URL地址判断当前所在页面
-  var pageArg = getarg(window.location.href)
+  var pageArg = getarg(window.location.href).split('&')[0]
   // 从配置项中取出程序入口
   var page = pageArg ? pageArg : globalConfig.entry
   if (page) {
@@ -132,31 +145,123 @@ window.onload = function() {
 window.onhashchange = function(e) {
   var oldUrlParam = getarg(e.oldURL)
   var newUrlParam = getarg(e.newURL)
-  // 查找页面跳转前的page页(dom节点)
-  // console.log(oldUrlParam)
-  // 如果源地址获取不到 那么一般是因为源页面为首页
-  if (oldUrlParam === undefined) {
-    oldUrlParam = globalConfig.entry
-  }
-  var oldDom = document.getElementById('ox-' + oldUrlParam)
+  // 切换页面特效
+  switchPage(oldUrlParam, newUrlParam)
+}
+// 页面切换效果
+
+// 获取URL参数
+function getQueryString(newUrlParam, name) { 
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
+  var r = newUrlParam.match(reg)
+  if (r != null) return unescape(r[2])
+  return null; 
+}
+
+// 无特效翻页
+function dispalyEffect (oldDom, newDom) {
   if (oldDom) {
     // 隐藏掉旧的节点
     oldDom.style.display = 'none'
   }
   // 查找页面跳转后的page
-  
-  var newDom = document.getElementById('ox-' + newUrlParam)
-  // console.log(newDom)
-  if (newDom) {
-    // 隐藏掉旧的节点
-    newDom.style.display = 'block'
-  } else {
+  newDom.style.display = 'block'
+}
+
+// 淡入效果
+function fadeinEffect (oldDom, newDom) {
+  // 查找页面跳转后的page
+  newDom.style.position = 'fixed'
+  newDom.style.left = 0
+  newDom.style.top = 0
+  newDom.style.width = '100%'
+  newDom.style.height = '100%'
+  newDom.style.opacity = 0
+  newDom.style.transition = 'opacity 1s'
+  newDom.style.display = 'block'
+  setTimeout(() => {
+    if (oldDom) {
+      // 隐藏掉旧的节点
+      oldDom.style.display = 'none'
+      // 清除临时设置的style
+      newDom.style.position = ''
+      newDom.style.left = ''
+      newDom.style.top = ''
+      newDom.style.width = ''
+      newDom.style.height = ''
+      newDom.style.opacity = ''
+      newDom.style.transition = ''
+    }
+  }, 900)
+  setTimeout(() => {
+    newDom.style.opacity = 1
+  }, 0)
+}
+
+// 淡出效果
+function fadeoutEffect (oldDom, newDom) {
+  // 查找页面跳转后的page
+  oldDom.style.zIndex = 999
+  oldDom.style.position = 'fixed'
+  oldDom.style.left = 0
+  oldDom.style.top = 0
+  oldDom.style.width = '100%'
+  oldDom.style.height = '100%'
+  oldDom.style.opacity = 1
+  oldDom.style.transition = 'opacity 1s'
+  newDom.style.display = 'block'
+  setTimeout(() => {
+    if (oldDom) {
+      // 隐藏掉旧的节点
+      oldDom.style.display = 'none'
+      // 清除临时设置的style
+      oldDom.style.position = ''
+      oldDom.style.left = ''
+      oldDom.style.top = ''
+      oldDom.style.width = ''
+      oldDom.style.height = ''
+      oldDom.style.opacity = ''
+      oldDom.style.transition = ''
+    }
+  }, 900)
+  setTimeout(() => {
+    oldDom.style.opacity = 0
+  }, 0)
+}
+
+function switchPage (oldUrlParam, newUrlParam) {
+  var oldPage = oldUrlParam.split('&')[0]
+  var newPage = newUrlParam.split('&')[0]
+  // 查找页面跳转前的page页(dom节点)
+  // console.log(oldUrlParam)
+  // 如果源地址获取不到 那么一般是因为源页面为首页
+  if (oldPage === undefined) {
+    oldPage = globalConfig.entry
+  }
+  var oldDom = document.getElementById('ox-' + oldPage)
+  var newDom = document.getElementById('ox-' + newPage)
+  if (!newDom) {
     console.error('页面不存在!')
     return
   }
-  window.ozzx.activePage = newUrlParam
-  runPageFunction(newUrlParam, newDom)
+  // 判断是否有动画效果
+  switch (getQueryString(newUrlParam, 'animation')) {
+    case null: {
+      dispalyEffect(oldDom, newDom)
+      break
+    }
+    case 'fadein': {
+      fadeinEffect(oldDom, newDom)
+      break
+    }
+    case 'fadeout': {
+      fadeoutEffect(oldDom, newDom)
+      break
+    }
+  }
+  
+  window.ozzx.activePage = newPage
+  runPageFunction(newPage, newDom)
 }
-
       window.ozzx.script = {home:{data:{nameList:{rank1:{name:"lis",like:"orange"},rank2:{name:"kim",like:"yellow"},rank3:{name:"tony",like:"white"}}},created:function created(){console.log('hellow word!');},methods:{showAlert:function showAlert(othersName,myName){console.log('可以传递参数:',othersName,myName);console.log('方便的获取到可能会用到的信息!');console.log(this);}}},name:{created:function created(){console.log('my name is pack!');}}}
     
