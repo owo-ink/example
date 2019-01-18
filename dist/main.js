407269a5787af4583a1843fcfa943555663d3188
PUGE <mail@puge.me>
2019-01-18 17:03:27 +0800

    save

--- dist/main.js
+++ dist/main.js
@@ -24,12 +24,17 @@ function runPageFunction (pageName, entryDom) {
   // 判断页面是否有自己的方法
   var newPageFunction = window.ozzx.script[pageName]
   if (!newPageFunction) return
-  // 注入运行环境
-  newPageFunction.created.apply(assign(newPageFunction, {
-    $el: entryDom,
-    activePage: window.ozzx.activePage,
-    domList: window.ozzx.domList
-  }))
+  console.log(newPageFunction)
+  // 如果有created方法则执行
+  if (newPageFunction.created) {
+    // 注入运行环境
+    newPageFunction.created.apply(assign(newPageFunction, {
+      $el: entryDom,
+      activePage: window.ozzx.activePage,
+      domList: window.ozzx.domList
+    }))
+  }
+  
   // 判断页面是否有下属模板,如果有运行所有模板的初始化方法
   for (var key in newPageFunction.template) {
     var templateScript = newPageFunction.template[key]
@@ -168,67 +173,6 @@ function dispalyEffect (oldDom, newDom) {
   newDom.style.display = 'block'
 }
 
-// 淡入效果
-function fadeinEffect (oldDom, newDom) {
-  // 查找页面跳转后的page
-  newDom.style.position = 'fixed'
-  newDom.style.left = 0
-  newDom.style.top = 0
-  newDom.style.width = '100%'
-  newDom.style.height = '100%'
-  newDom.style.opacity = 0
-  newDom.style.transition = 'opacity 1s'
-  newDom.style.display = 'block'
-  setTimeout(() => {
-    if (oldDom) {
-      // 隐藏掉旧的节点
-      oldDom.style.display = 'none'
-      // 清除临时设置的style
-      newDom.style.position = ''
-      newDom.style.left = ''
-      newDom.style.top = ''
-      newDom.style.width = ''
-      newDom.style.height = ''
-      newDom.style.opacity = ''
-      newDom.style.transition = ''
-    }
-  }, 900)
-  setTimeout(() => {
-    newDom.style.opacity = 1
-  }, 0)
-}
-
-// 淡出效果
-function fadeoutEffect (oldDom, newDom) {
-  // 查找页面跳转后的page
-  oldDom.style.zIndex = 999
-  oldDom.style.position = 'fixed'
-  oldDom.style.left = 0
-  oldDom.style.top = 0
-  oldDom.style.width = '100%'
-  oldDom.style.height = '100%'
-  oldDom.style.opacity = 1
-  oldDom.style.transition = 'opacity 1s'
-  newDom.style.display = 'block'
-  setTimeout(() => {
-    if (oldDom) {
-      // 隐藏掉旧的节点
-      oldDom.style.display = 'none'
-      // 清除临时设置的style
-      oldDom.style.position = ''
-      oldDom.style.left = ''
-      oldDom.style.top = ''
-      oldDom.style.width = ''
-      oldDom.style.height = ''
-      oldDom.style.opacity = ''
-      oldDom.style.transition = ''
-    }
-  }, 900)
-  setTimeout(() => {
-    oldDom.style.opacity = 0
-  }, 0)
-}
-
 function switchPage (oldUrlParam, newUrlParam) {
   var oldPage = oldUrlParam
   var newPage = newUrlParam
@@ -257,40 +201,33 @@ function switchPage (oldUrlParam, newUrlParam) {
       dispalyEffect(oldDom, newDom)
       return
     }
-    console.log(animationIn, animationOut)
+    oldDom.style.position = 'absolute'
+
     newDom.style.display = 'block'
-    newDom.style.position = 'fixed'
-    newDom.style.left = 0
-    newDom.style.top = 0
-    newDom.style.width = '100%'
-    newDom.style.height = '100%'
-    document.body.style.overflow = 'hidden'
+    newDom.style.position = 'absolute'
+    // document.body.style.overflow = 'hidden'
     animationIn.split(',').forEach(value => {
       oldDom.classList.add('ox-page-' + value)
     })
     animationOut.split(',').forEach(value => {
       newDom.classList.add('ox-page-' + value)
     })
-    
-    setTimeout(() => {
+    oldDom.addEventListener("animationend", function() {
       // 隐藏掉旧的节点
       oldDom.style.display = 'none'
-      // 清除临时设置的style
-      newDom.style.position = ''
-      newDom.style.left = ''
-      newDom.style.top = ''
-      newDom.style.width = ''
-      newDom.style.height = ''
-
+      // oldDom.style.position = ''
       // 清除临时设置的class
       animationIn.split(',').forEach(value => {
         oldDom.classList.remove('ox-page-' + value)
       })
+    })
+    newDom.addEventListener("animationend", function() {
+      // 清除临时设置的style
+      // newDom.style.position = ''
       animationOut.split(',').forEach(value => {
         newDom.classList.remove('ox-page-' + value)
       })
-      document.body.style.overflow = ''
-    }, 2000)
+    })
   } else {
     dispalyEffect(oldDom, newDom)
   }
@@ -298,5 +235,5 @@ function switchPage (oldUrlParam, newUrlParam) {
   window.ozzx.activePage = newPage
   runPageFunction(newPage, newDom)
 }
-      window.ozzx.script = {home:{data:{nameList:{rank1:{name:"lis",like:"orange"},rank2:{name:"kim",like:"yellow"},rank3:{name:"tony",like:"white"}}},created:function created(){console.log('hellow word!');},methods:{showAlert:function showAlert(othersName,myName){console.log('可以传递参数:',othersName,myName);console.log('方便的获取到可能会用到的信息!');console.log(this);this.$el.innerText="Hellow ".concat(othersName,", My name is ").concat(myName);}}},name:{created:function created(){console.log('my name is pack!');}}}
+      window.ozzx.script = {home:{data:{nameList:{rank1:{name:"lis",like:"orange"},rank2:{name:"kim",like:"yellow"},rank3:{name:"tony",like:"white"}}},created:function created(){console.log('hellow word!');},methods:{showAlert:function showAlert(othersName,myName){console.log('可以传递参数:',othersName,myName);console.log('方便的获取到可能会用到的信息!');console.log(this);this.$el.innerText="Hellow ".concat(othersName,", My name is ").concat(myName);}}},name:{created:function created(){console.log('my name is pack!');},template:{topBar:{methods:{return:function _return(){window.history.go(-1);}}}}},animation:{template:{topBar:{methods:{return:function _return(){window.history.go(-1);}}}}}}
     
